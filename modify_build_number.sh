74e28b5943850fd5ca5e4dcf58a84f44694db036
hechao <1054134542@qq.com>
2019-06-19 09:41:40 +0800

    修改脚本，修复交叉打不同版本的包的时候，修改build号异常的bug

--- modify_build_number.sh
+++ modify_build_number.sh
@@ -3,12 +3,20 @@
 export LC_ALL=en_US.UTF-8
 
 source ~/.bashrc
-# 打包本地存储的打包信息json路径
-BuildJsonPath=$1
-echo ${BuildJsonPath}
+# 存储所有版本号的打包版本信息文件夹【一个版本对应一个json避免一个json导致打了不同的版本的包覆盖的情况】，这个文件夹由外部传入，一般放到Jenkins的主目录
+BuildNumberPath=$1
+if [[ ! -d $BuildNumberPath ]]; then
+    mkdir $BuildNumberPath
+    chmod 777 $BuildNumberPath
+    echo ${BuildNumberPath}
+else
+    echo ${BuildNumberPath} 
+fi
 # 版本号自增
-PlistDir='YourAppInfo.plist dir'
+PlistDir="YourProjectInfoPlistPath"
 BuildVersion=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "${PlistDir}")
+BuildJsonPath=${BuildNumberPath}/${BuildVersion}.json
+echo ${BuildJsonPath}
 if [[ -f $BuildJsonPath ]]; then
 # 读取Json中上次打包的build信息
     echo "读取Json中上次打包的build信息"
@@ -25,21 +33,21 @@ else
 fi
 
 if [ "${PreBuildVersion}" == "${BuildVersion}" ]; then
-	echo "版本号未改变"
+    echo "版本号未改变"
     BuildNumber=`expr ${PreBuildNumber} + 1`
 else
-	echo "版本号改变"
+    echo "版本号改变"
     BuildNumber=$(/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" "${PlistDir}")
-	echo "BuildNumber:${BuildNumber}"
+    echo "BuildNumber:${BuildNumber}"
     BuildNumber=`expr ${BuildNumber} + 1`
 fi
 
 echo "自增之后BuildNumber:${BuildNumber}"
 
 if [[ "${BuildNumber}" != "" ]]; then
-	# 修改Info.plist的build号
-	echo "修改Info.plist的build号"
-	/usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${BuildNumber}" "${PlistDir}"
-	# 写本次打包信息到build.json
-	echo "{\"BuildNumber\":\"${BuildNumber}\", \"BuildVersion\":\"${BuildVersion}\"}" > ${BuildJsonPath}
+    # 修改Info.plist的build号
+    echo "修改Info.plist的build号"
+    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${BuildNumber}" "${PlistDir}"
+    # 写本次打包信息到build.json
+    echo "{\"BuildNumber\":\"${BuildNumber}\", \"BuildVersion\":\"${BuildVersion}\"}" > ${BuildJsonPath}
 fi
